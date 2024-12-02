aacf5f83884b80813aa436f3ae853ec8ba037013
Md Moazz <moazzemh222@gmail.com>
2024-12-02 15:51:37 +0600

    Abstract class

---
+++ Abstract Class/Abstract Class/Program.cs
@@ -0,0 +1,46 @@
+﻿//Abstact class
+abstract class Animal
+{
+    //Abstract method(no impliment)
+    public abstract void MakeSound();
+
+    //Regular method(has implimentation)
+    public void Sleep()
+    {
+        Console.WriteLine("This animal is sleeping");
+    }
+
+    //Child class
+    class Dog : Animal
+    {
+        //Implementing the abstract method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+    class Cat : Animal
+    {
+        //Implementing the abstract method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Cat mew");
+        }
+    }
+
+    //Using the classes
+    class Program
+    {
+        static void Main()
+        {
+            Animal myDog = new Dog();
+            myDog.MakeSound();
+            myDog.Sleep();
+
+            Animal myCat = new Cat();
+            myCat.MakeSound();
+            myCat.Sleep();
+        }
+    }
+}
