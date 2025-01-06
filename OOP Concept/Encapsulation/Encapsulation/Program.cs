2511eb9fc44f0a3044842f9078ab5dfffb22898d
Md Moazz <moazzemh222@gmail.com>
2025-01-06 11:35:06 +0600

    Encapsulation

---
+++ OOP Concept/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿class Person
+{
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+                name = value;
+            else
+                throw new ArgumentException("Name cannot be emplty.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person();
+        person.Name = "John";
+        Console.WriteLine(person.Name);
+    }
+}
+
+
