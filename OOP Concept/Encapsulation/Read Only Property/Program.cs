cca9f33520c7626880bd842405465cb2045cf44e
Md Moazz <moazzemh222@gmail.com>
2025-01-07 23:50:15 +0600

    Encapsulation read only

---
+++ OOP Concept/Encapsulation/Read Only Property/Program.cs
@@ -0,0 +1,21 @@
+﻿class Circle
+{
+    private double radius;
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+    public double Area
+    {
+        get { return Math.PI * radius * radius; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Circle circle = new Circle(5);
+        Console.WriteLine($"Area: {circle.Area}");
+    }
+}
